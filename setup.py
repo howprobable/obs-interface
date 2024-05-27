62379ee987abbe80ac49f49d46fa92d09ff2089a
howprobable <69045100+howprobable@users.noreply.github.com>
2024-05-27 22:01:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        "obswebsocket",
+        "obs-websocket-py",
         "subprocess",
         "pyautogui",
         "psutil",
